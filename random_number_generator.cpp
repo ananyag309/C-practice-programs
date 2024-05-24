719db8a65673a8fce8129b584242eace3afa6737
Ananya Gupta <145869907+ananyag309@users.noreply.github.com>
2024-05-24 12:52:32 +0530

    Add files via upload

---
+++ random_number_generator.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+int main()
+{
+
+    // Rolling 6 sided dice three times
+    srand(time(NULL));
+
+    int num1 = (rand() % 6) + 1;
+    int num2 = (rand() % 6) + 1;
+    int num3 = (rand() % 6) + 1;
+
+    std::cout << num1 << '\n';
+    std::cout << num2 << '\n';
+    std::cout << num3 << '\n';
+
+    return 0;
+
+}
